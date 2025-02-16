81f6c958791868e3984080e2cfcab3ca86e25201
Ryan Ashton <djryanash@me.com>
2025-02-16 11:51:19 +0800

    add routes

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
-package main 
+package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 // album represents data about a record album.
 type album struct {
@@ -17,7 +21,50 @@ var albums = []album{
   {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID)
+	router.POST("/albums", postAlbums)
+
+	router.Run("localhost:8080")
+}
+
 // getAlbums responds with the list of all albums.
-func getAlbums(c *gin.Context) {
+func getAlbums(context *gin.Context) {
+	context.IndentedJSON(http.StatusOK, albums)
+}
+
+// postAlbums adds an album from JSON to newAlbum
+func postAlbums(c *gin.Context) {
 	
+	var newAlbum album
+
+	// Call BindJSON to bind the received JSON to a newAlbum.
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return 
+	}
+
+	// Add the new album to the slice 
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+
+}
+
+// getAlbumByID locates the album whose ID value matches the ID
+// parameter sent by the client, then returns that album as a response.
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	// Loop over the list of albums looking for
+	// an album whos ID value matches the parameter.
+
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return 
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+
 }
